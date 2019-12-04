4f9cb86d13fdc51184662fa8ab77a75d0a3d54d1
Jeremy R <jeremy.rougeau@aletheiainteractive.com>
2019-12-04 06:46:11 +0000

    Done.

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,19 @@
 class Waiter
 
+attr_accessor :name, :years
+
+@@all = []
+
+
+def initialize(name, years)
+@name = name
+@years = years
+@@all << self
+end
+
+def self.all 
+  @@all 
+end
+
+
 end
